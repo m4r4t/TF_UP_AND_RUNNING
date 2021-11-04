fcbcf48cf91164ee1aea76a718804359a5bbd481
Marat Namazbayev <marat.namazbayev@gmail.com>
2021-11-04 16:30:02 +0600

    Add egress rule for downloading updates

--- main.tf
+++ main.tf
@@ -36,4 +36,11 @@ resource "aws_security_group" "web_server_sg" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "instance_name" {
     description = "The name of the instance"
+    type = string
 }
 
 variable "server_port" {
   description = "Http port for busybox"
+  type = number
 }
 
 variable "kp" {
   description = "Key pair name"
+  type = string
 }
